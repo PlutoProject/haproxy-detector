a21a09f1ed5a9355bfa7e3c8e16c5f2ea6e54938
nostalfinals <yuu8583@proton.me>
2024-03-30 23:24:16 +0800

    build: 使用 Java 21 & 更换到 Gradle 与更新依赖

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("java")
+    id("idea")
+    id("io.github.goooler.shadow") version "8.1.7"
+}
+
+group = "net.andylizi.haproxydetector"
+version = "4.0.0"
+
+val bStatsVersion = "3.0.2"
+
+repositories {
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://repo.dmulloy2.net/repository/public")
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+dependencies {
+    compileOnly("io.netty:netty-codec-haproxy:4.1.108.Final")
+    implementation("commons-validator:commons-validator:1.8.0")
+    implementation("org.bstats:bstats-bukkit:$bStatsVersion")
+    implementation("org.bstats:bstats-bungeecord:$bStatsVersion")
+    implementation("org.bstats:bstats-velocity:$bStatsVersion")
+    implementation("org.bstats:bstats-velocity:$bStatsVersion")
+    compileOnly("io.github.waterfallmc:waterfall-api:1.20-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
+    compileOnly("com.velocitypowered:velocity-api:3.3.0-SNAPSHOT")
+    annotationProcessor("com.velocitypowered:velocity-api:3.3.0-SNAPSHOT")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("org.junit.platform:junit-platform-launcher:1.10.2")
+}
